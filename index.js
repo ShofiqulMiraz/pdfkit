7387d181ae44a989448aff57b9350cc8593b55da
Shofiqul Islam Miraz <simiraz90@gmail.com>
2024-02-06 13:26:06 +0000

    Refactor dashed line and add text in generatePdf function

--- index.js
+++ index.js
@@ -113,10 +113,20 @@ const generatePdf = async (payload) => {
     doc.text(`Order: ${order_id}`, { align: "left" });
 
     // dashed line
+    let xStart = 10;
+    let xEnd = 287.64;
+    let yPosition = show_table ? 170 : 155;
+    let middlePoint = xStart + (xEnd - xStart) / 2;
+    let textOffset = 10; // adjust this value to move the text up or down
+
     if (show_table) {
-        doc.moveTo(10, 170).lineTo(287.64, 170).dash(5, { space: 2 }).opacity(1).stroke();
+        doc.moveTo(xStart, yPosition + textOffset).lineTo(middlePoint - 20, yPosition + textOffset).dash(5, { space: 2 }).opacity(1).stroke();
+        doc.text('Your Text', middlePoint, yPosition, { width: 90, align: 'center' });
+        doc.moveTo(middlePoint + 20, yPosition + textOffset).lineTo(xEnd, yPosition + textOffset).dash(5, { space: 2 }).opacity(1).stroke();
     } else {
-        doc.moveTo(10, 155).lineTo(287.64, 155).dash(5, { space: 2 }).opacity(1).stroke();
+        doc.moveTo(xStart, yPosition + textOffset).lineTo(middlePoint - 20, yPosition + textOffset).dash(5, { space: 2 }).opacity(1).stroke();
+        doc.text('Your Text', middlePoint, yPosition, { width: 90, align: 'center' });
+        doc.moveTo(middlePoint + 20, yPosition + textOffset).lineTo(xEnd, yPosition + textOffset).dash(5, { space: 2 }).opacity(1).stroke();
     }
 
     // end doc and return it
@@ -125,7 +135,7 @@ const generatePdf = async (payload) => {
     return doc;
 };
 
-app.post("/print", async (req, res) => {
+app.get("/print", async (req, res) => {
     // call generatePdf function
     const doc = await generatePdf({
         logo: "https://c9devstorage.blob.core.windows.net/c9-pos/c9-pos-f6d97551-0145-473e-8d0b-c92e7b264a45-jpeg",
@@ -144,12 +154,14 @@ app.post("/print", async (req, res) => {
     });
 
     // See below for browser usage
-    doc.pipe(fs.createWriteStream("output.pdf"));
+    // doc.pipe(fs.createWriteStream("output.pdf"));
+    doc.pipe(res);
 
     // return success
-    res.status(200).json({
-        message: "success",
-    });
+    // res.status(200).json({
+    //     message: "success",
+
+    // });
 });
 
 app.listen(8080, () => {
